c138200cc7490ee35bc65ed217e02e1d423ce8a3
reilo <rl@lorein.de>
2016-03-15 21:40:06 +0100

    layout.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,14 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-    'ngRoute',
-    'myApp.playersView',
-    'myApp.tournamentsView',
-    'myApp.version'
-]).constant('ENDPOINT_URI', 'http://localhost:3000/api').config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.otherwise({redirectTo: '/playersView'})
-}]);
+        'ngRoute',
+        'myApp.playersView',
+        'myApp.tournamentsView',
+        'myApp.version'
+    ])
+
+    .constant('ENDPOINT_URI', 'http://localhost:3000/api')
+
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/playersView'})
+    }]);
